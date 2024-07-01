e511346970ec75f05a66125ee638ef436025f35d
nicolasfrouin-dgrp <nicolas.f@dental-groupe.com>
2024-07-01 16:18:57 +0200

    change favicon (tmp)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,29 +24,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
+        {/* <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/favicon/apple-touch-icon.png"
-        />
-        <link
+        /> */}
+        {/* <link
           rel="icon"
           type="image/png"
           sizes="32x32"
           href="/favicon/favicon-32x32.png"
-        />
-        <link
+        /> */}
+        {/* <link
           rel="icon"
           type="image/png"
           sizes="16x16"
           href="/favicon/favicon-16x16.png"
-        />
+        /> */}
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
+        {/* <link
           rel="mask-icon"
           href="/favicon/safari-pinned-tab.svg"
           color="#000000"
-        />
+        /> */}
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta
